943fced2594fce0e1114dce43d32c4f821db788c
SamyamCX <samyamrajb@gmail.com>
2025-05-19 06:28:58 +0545

    Added custom cursor

--- assests/script.js
+++ assests/script.js
@@ -24,3 +24,10 @@ function toggleMenu() {
   // Set the initial volume of the background music
   const audioElement = document.getElementById('background-music');
   audioElement.volume = 0.5; // Set the volume to 20% of the max volume (0.0 - 1.0)
+
+  const customCursor = document.getElementById("custom-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  customCursor.style.left = `${e.clientX}px`;
+  customCursor.style.top = `${e.clientY}px`;
+});
